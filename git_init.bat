bc2829783f60ec9737ac52ab1f610f47847b1de7
Musa <sparklle.shop.pk@gmail.com>
2026-05-15 13:12:47 +0500

    Initial commit - Secured project

--- git_init.bat
+++ git_init.bat
@@ -1,3 +1,7 @@
 git init
 git add .
-git commit -m "initial commit"
+git commit -m "Initial commit - Secured project"
+git branch -M main
+git remote add origin https://github.com/sparklleshoppk-create/demo-website-chatthas.git
+git push -u origin main
+pause
